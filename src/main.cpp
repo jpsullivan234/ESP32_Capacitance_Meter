d4b5add720fb36df6a18ea9acb5a3b6ae4459f8e
John Sullivan <jpsullivan234@icloud.com>
2024-03-22 16:09:00 -0500

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <vector>
 
 #define debugger 1
 #if debugger == 1
@@ -19,7 +18,6 @@ struct doubleLong
 
 // Functions
 doubleLong measureADC(int num_measurements, byte charge_pin, byte sense_pin, byte charge_time_us, byte discharge_time_ms);
-float polyVal(int size, const float *map, float val);
 
 // Variables
 const byte sensePin = 3;
@@ -33,14 +31,8 @@ const uint16_t MAX_ADC_VALUE = 4095;
 const float resistor_mohm = 2.035;
 const float Vref = 5.0;
 
-const int size = 2;
-const float mappingCurve[size] = {2.1049e+5,-5.5683e+1};
-const float mappingCurveTop[size] = {1.7964e+5,-1.7368e+1};
-const int switchMapBelow = 880;
-
 float capacitance_pf = 0.0;
 float capVoltage = 0.0;
-float height_mm = 0.0;
 
 
 void setup() {
@@ -70,22 +62,8 @@ void loop() {
   capacitance_pf = -1.0 * ((chargeTime_us) / resistor_mohm ) / log(1 - (capVoltage / Vref));
   if (capacitance_pf > 0.0) capacitance_pf -= 9.0; // -= 9.0 when charge time is 30
 
-  // calculate the water level
-  if (ADCvalues.charged_value < switchMapBelow)  // Use the equation for the top of the bottle
-  {
-    height_mm = polyVal(size, mappingCurveTop, 1.0/ADCvalues.charged_value);
-  }
-  else 
-  {
-    height_mm = polyVal(size, mappingCurve, 1.0/ADCvalues.charged_value);
-  }
-
   // capacitance is 8 pf too high (probably due to parasitic cap of breadboard)
-  debug(capacitance_pf ); debug("pf | "); debug(ADCvalues.charged_value); debug(" | "); debug(ADCvalues.discharged_value);
-  debug(" | "); debug(height_mm); debugln("mm");
-  //debug(ADCvalues.charged_value); debug(" | "); debugln(ADCvalues.discharged_value);
-  //*/
-
+  debug(capacitance_pf ); debug("pf | "); debug(ADCvalues.charged_value); debug(" | "); debugln(ADCvalues.discharged_value);
 
   delay(1);
 }
@@ -115,13 +93,3 @@ doubleLong measureADC(
   }
   return {charged_val /= num_measurements, discharged_val /= num_measurements};
 }
-
-float polyVal(int size, const float *map, float val)  // takes a uint16_t value and finds the corresponding y value from the specified curve
-{
-  float temp = 0;
-  for (int i = 0; i < size; i++){
-    temp += (map[i] * pow(val,size -1 -i));
-  }
-  return temp;
-}
-
